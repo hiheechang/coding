f350c6cfdbfa8af20dc44e2f158a28e1df7fe636
heechang1997@gmail.com <heechang1997@DESKTOP-J5BTOTB.localdomain>
2019-08-06 15:45:20 +0900

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+const nemo = ['nemo'];
+
+function findNemo(array) {
+    for (let i =0; i<array.length; ++i){
+        if(array[i] === 'nemo'){
+            console.log('found nemo');
+        }
+    }
+
