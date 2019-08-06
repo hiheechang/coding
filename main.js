55e44b1c8621e27d8164cc489ef00c5fabd6a676
heechangkang <heechang1997@gmail.com>
2019-08-06 21:48:06 +0900

    completed the draft

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+const fish = ['dory', 'bruce', 'marlin', 'nemo'];
 const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank'];
+const large = new Array(10).fill('nemo');
 
-function findNemo(array) {
-    for (let i =0; i<array.length; ++i){
-        if(array[i] === 'nemo'){
-            console.log('found nemo');
-        }
+function findNemo2(fish) {
+  let t0 = performance.now();
+    for (let i = 0; i < fish.length; i++) {
+      if (fish[i] === 'nemo') {
+        console.log('Found NEMO!');
+      }
     }
+  let t1 = performance.now();
+	console.log("Call to find Nemo took " + (t1 - t0) + " milliseconds.");
+}
 
+findNemo2(everyone);
